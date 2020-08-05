04601c0f00a8fa94839d7d835aeaaa6982ef0f85
Angel Gonzalez <angeledugo@gmail.com>
2020-08-05 18:28:25 -0500

    actualizacion conexion

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 const io = socketio.listen(server);
 
 // Conexion a la base de datos
-mongoose.connect('mongodb://localhost/chat-database')
+mongoose.connect('mongodb://localhost/chat-database',{ useNewUrlParser: true,useUnifiedTopology: true })
     .then(db => console.log('db is conected'))
     .catch(err => console.log(err));
 
